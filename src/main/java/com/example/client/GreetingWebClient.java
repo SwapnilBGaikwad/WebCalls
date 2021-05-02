b720d3f0c93d54a39f2c9e7e863f0e3ecf99d07d
Swapnil Gaikwad <swapnil.gaikwad@PP-C02Y77B3JG5H.local>
2021-05-02 15:30:04 +0530

    Add details

---
+++ src/main/java/com/example/client/GreetingWebClient.java
@@ -0,0 +1,38 @@
+package com.example.client;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+import reactor.netty.http.client.HttpClient;
+import reactor.netty.resources.ConnectionProvider;
+
+import java.time.Duration;
+
+public class GreetingWebClient {
+    private WebClient webClient;
+
+    public GreetingWebClient() {
+        ConnectionProvider connectionProvider = ConnectionProvider.builder("SamplePool")
+                .maxConnections(100)
+                .maxLifeTime(Duration.ofMinutes(3L))
+                .build();
+
+        HttpClient httpClient = HttpClient.create(connectionProvider);
+        webClient = WebClient.builder()
+                .clientConnector(new ReactorClientHttpConnector(httpClient))
+                .baseUrl("http://localhost:8080")
+                .build();
+    }
+
+    public String getResult() {
+        Mono<ResponseEntity<String>> result = webClient.get()
+                .uri("/hello")
+                .accept(MediaType.TEXT_PLAIN)
+                .retrieve()
+                .toEntity(String.class);
+        return ">> result = " + result.map(HttpEntity::getBody).block();
+    }
+}
