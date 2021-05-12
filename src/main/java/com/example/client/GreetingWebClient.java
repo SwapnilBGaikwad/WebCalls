40c4212760747bd3aac701e42f8ef720eb9fa9ad
Swapnil Gaikwad <swapnil.gaikwad@PP-C02Y77B3JG5H.local>
2021-05-12 09:04:46 +0530

    Added Flux approach

--- src/main/java/com/example/client/GreetingWebClient.java
+++ src/main/java/com/example/client/GreetingWebClient.java
@@ -8,12 +8,17 @@ import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
 import reactor.netty.http.client.HttpClient;
 import reactor.netty.resources.ConnectionProvider;
 
 import java.time.Duration;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class GreetingWebClient {
     private WebClient webClient;
@@ -39,23 +44,28 @@ public class GreetingWebClient {
         }
         List<String> results = new ArrayList<>();
         List<List<String>> partitions = Lists.partition(list, 5);
-        for (List<String> partition : partitions) {
-            List<Mono<String>> finalList = new ArrayList<>();
-            for (String item : partition) {
-                finalList.add(getRes());
-            }
-            Mono<List<String>> listMono = Flux.merge(finalList).collectList();
-            results.addAll(listMono.block());
-        }
-        System.out.println("Completed request " + Thread.currentThread().getName());
-        return results;
-//        return Flux.fromIterable(list)
-//                .limitRate(5)
-//                .parallel()
-//                .runOn(Schedulers.parallel())
-//                .flatMap(a -> getRes())
-//                .sequential()
-//                .collectList();
+//        for (List<String> partition : partitions) {
+//            List<Mono<String>> finalList = new ArrayList<>();
+//            for (String item : partition) {
+//                finalList.add(getRes());
+//            }
+//            Mono<List<String>> listMono = Flux.merge(finalList).collectList();
+//            results.addAll(listMono.block());
+//        }
+//        System.out.println("Completed request " + Thread.currentThread().getName());
+//        return results;
+
+        Mono<List<List<String>>> listMono = Flux.fromIterable(partitions)
+                .flatMap(partition -> {
+                    List<Mono<String>> finalList = new ArrayList<>();
+                    for (String item : partition) {
+                        finalList.add(getRes());
+                    }
+                    return Flux.merge(finalList).collectList();
+                })
+                .collectList();
+        List<List<String>> block = listMono.block();
+        return block.stream().flatMap(Collection::stream).collect(Collectors.toList());
     }
 
     public Mono<String> getRes() {
